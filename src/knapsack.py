36999098732728249ed893727732599e2980ff3f
endorxmr <28117225+endorxmr@users.noreply.github.com>
2023-06-05 21:14:59 +0200

    Work with computer list in knapsack solver

--- src/knapsack.py
+++ src/knapsack.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import math
+import sunrise_lib
 
 from ortools.algorithms import pywrapknapsack_solver
 
 
-def solver(timeframe:int, power_limit:int, energy_limit:int|float) -> list[int]:
+def solver(devices:list[dict], timeframe:int, power_limit:int, energy_limit:int|float) -> list[dict]:
     """
     Calculate the mining solution for a given timeframe, power limit, and energy limit.
 
     Args:
+        devices (list(dict)): List of mining devices available.
         timeframe (int): Duration of the timeframe in minutes. [min]
         power_limit (int): Maximum power available for mining in Watts. [W]
         energy_limit (int|float): Energy available for mining in this timeframe in kilowatt-hours. [kWh]
 
     Returns:
-        list(int): A list of devices (represented as integers) that will be used for mining.
+        list(dict): List of mining devices that will be used.
 
     Raises:
         TypeError: If any of the input arguments are not of the expected type.
@@ -27,20 +29,22 @@ def solver(timeframe:int, power_limit:int, energy_limit:int|float) -> list[int]:
     # Type checking
     if not isinstance(timeframe, int) or not isinstance(power_limit, int) or not isinstance(energy_limit, int|float):
         raise TypeError("The 'timeframe' and 'power_limit' arguments must be of type 'int', and the 'energy_limit' argument must be of type 'int' or 'float'.")
+    if not isinstance(devices, list):
+        raise TypeError("'devices' must be a list of dicts")
 
     # Negative value checking
     if timeframe < 0 or power_limit < 0 or energy_limit < 0:
         raise ValueError("Input arguments cannot have negative values.")
-
-    # Read the computers configuration
-    # print(sunrise_lib.config_computers)
+    if len(devices) == 0:  # Empty list of devices, skipping computation and returning the same empty list
+        return devices
 
     # Create the solver.
     solver = pywrapknapsack_solver.KnapsackSolver(
         pywrapknapsack_solver.KnapsackSolver.
         KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'KnapsackExample')
     # TODO: figure out a way to ensure the minimum efficiency. An unconstrained solver might create a solution
-    # that drops below the minimum profitability, if there's enough power and energy available.
+    # that drops below the minimum profitability, if there's enough power and energy available. (Actually this
+    # can even happen at the lower end).
     # Maybe this could come as a modified power/energy constraint? i.e. calculate the maximum power/energy that
     # we can consume at the current breakeven efficiency in our timeframe, and use that as the limit if it's a
     # tighter constraint than the current available power/energy.
@@ -62,8 +66,11 @@ def solver(timeframe:int, power_limit:int, energy_limit:int|float) -> list[int]:
     # ACTUALLY, the power available might also change for each timeframe, because we have to take into account
     # any other external consumers!
 
-    hashrates = [1000, 1000, 2920, 2920, 2920, 680*12, 680*12, 680*12, 680*12, 680*12]  # H
-    powers = [46, 46, 7.3*4, 7.3*4, 7.3*4, 73.8, 73.8, 73.8, 73.8, 73.8]  # W
+    # hashrates = [1000, 1000, 2920, 2920, 2920, 680*12, 680*12, 680*12, 680*12, 680*12]  # H
+    # powers = [46, 46, 7.3*4, 7.3*4, 7.3*4, 73.8, 73.8, 73.8, 73.8, 73.8]  # W
+    device_indices = [j for j in range(len(devices)) for i in range(devices[j]["count"])]
+    hashrates = [device["hash_per_core"] * device["cores"] for device in devices for _ in range(device["count"])]
+    powers = [device["watt_per_core"] * device["cores"] for device in devices for _ in range(device["count"])]
     energy_const = timeframe / 60 / 1000  # [min] / 60 [min/h] / 1000 [W/kW]
     energies = [p * energy_const for p in powers]  # kWh
     # energies = powers  # W!!
@@ -103,8 +110,14 @@ def solver(timeframe:int, power_limit:int, energy_limit:int|float) -> list[int]:
     print('Packed items:', packed_items)
     print('Packed_weights:', packed_weights[0], packed_weights[1])
 
-    return packed_items
+    chosen_devices = [device_indices[x] for x in packed_items]
+    for i in range(len(devices)):
+        devices[i]['count'] = chosen_devices.count(i)
+    return devices
 
 
 if __name__ == '__main__':
-    solver(5, 250, 1)
+    # Read the computers configuration
+    computers = list(sunrise_lib.config_computers.get('computers'))
+
+    print(solver(computers, 5, 250, 1))
