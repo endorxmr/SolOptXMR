fb353d964a7912ff219aef5a3360a07babc13bc3
endorxmr <28117225+endorxmr@users.noreply.github.com>
2023-07-10 21:47:42 +0200

    Update old pip package versions in deps-pull.sh

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -20,10 +20,10 @@ if pip3 install -r requirements.txt ; then
 else
 	echo "pip failed. Trying a fallback."
 	pip3 uninstall -r requirements.txt -y
-	pip3 install pandas>=1.4.2 scipy>=1.2.0 numpy>=1.19.3 matplotlib \
+	pip3 install pandas>=2.0.3 scipy>=1.11.1 numpy>=1.25.1 matplotlib \
 	pvlib pykrakenapi pyparsing pyrsistent python-dateutil \
 	python-json-config pytz requests six urllib3 \
-	beautifulsoup4 wget cairosvg Pillow>=9.1.1 geocoder \
+	beautifulsoup4 wget cairosvg Pillow>=9.2.0 geocoder \
 	opencv-python screeninfo ortools \
 	# pytesseract not employed yet
 	#pip3 install --upgrade 
