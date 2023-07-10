7afe9c5042ad2cfe4f05d84935d6633d39920b31
endorxmr <28117225+endorxmr@users.noreply.github.com>
2023-07-10 17:46:36 +0200

    Disable ci script to build opti in 'minimal' ci

--- util/ci.sh
+++ util/ci.sh
@@ -17,11 +17,11 @@ python3 src/tests.py
 python3 src/tests.py
 
 #util/build-debian.sh
-if [ "$(uname)" == "Linux" ]; then
-./ci-default -o "VERBOSE_FUNCTIONS=OFF"
-else
-./ci-default -o "VERBOSE_FUNCTIONS=ON"
-fi
+# if [ "$(uname)" == "Linux" ]; then
+# ./ci-default -o "VERBOSE_FUNCTIONS=OFF"
+# else
+# ./ci-default -o "VERBOSE_FUNCTIONS=ON"
+# fi
 
 if [ $# -eq 1 ]; then
 	echo "Production test not requested. Exiting."
@@ -30,12 +30,13 @@ fi
 echo "Testing the entire production chain:"
 # TODO: Make optional & derive a build script from tsqsim/util/build.py:
 export R_HOME=/usr/lib/R && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
-if [ "$(uname)" == "Linux" ]; then
-	python3 src/prod.py
-else
-	# TODO: Mac OSX suffers from an endless loop (?) in prod.py
-	#python3 src/prod.py
-	cd build/default-static-release/bin/
-	./opti # --out "/tmp" # todo: should be defaulted in the CI App's CLI
-fi
+python3 src/prod.py
+# if [ "$(uname)" == "Linux" ]; then
+# 	python3 src/prod.py
+# else
+# 	# TODO: Mac OSX suffers from an endless loop (?) in prod.py
+# 	#python3 src/prod.py
+# 	cd build/default-static-release/bin/
+# 	./opti # --out "/tmp" # todo: should be defaulted in the CI App's CLI
+# fi
 
